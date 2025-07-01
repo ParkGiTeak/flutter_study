459bc3da89e6a6d0aa2713116eeae331c8fa05da
ParkGiTeak <rl980901@naver.com>
2025-07-01 10:41:54 +0900

    feat: TextField, Image Widget Study

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -8,12 +8,12 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  int count = 0;
+  int count = 10;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('홈')),
+      appBar: AppBar(title: Text('카운터')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -45,6 +45,27 @@ class _MainPageState extends State<MainPage> {
             OutlinedButton(
               onPressed: () {},
               child: Text('OutlinedButton'),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: '글자', // 클릭전 힌트 형식에서 누르면 상단으로 이동
+                border: OutlineInputBorder(), // 외곽선
+              ),
+              onChanged: (text) {
+                print(text);
+              },
+            ),
+            Image.network(
+              'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT15RrII6lkLU_aF06n_JjKUxYKIW_iPqwlj1cDw19tWV1asbxquziiHB6b34qqvDMKYLRD_g',
+              width: 100,
+              height: 100,
+              fit: BoxFit.fill,
+            ),
+            Image.asset(
+              'assets/image.png',
+              width: 100,
+              height: 100,
+              fit: BoxFit.fill,
             ),
           ],
         ),
