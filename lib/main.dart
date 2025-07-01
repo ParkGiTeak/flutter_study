5a40f5257439f787844c6c5ad2a6f9c1cd1d16f7
ParkGiTeak <rl980901@naver.com>
2025-07-01 10:18:41 +0900

    rename: MyHomePage -> MainPage로 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'my_home_page.dart';
+import 'main_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(),
+      home: const MainPage(),
     );
   }
 }
