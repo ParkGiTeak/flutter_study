7340072bbe5ef3b842541779697a7f0770579eb6
ParkGiTeak <rl980901@naver.com>
2025-07-01 10:50:34 +0900

    feat: Container, SizedBox, SingleChildScrollView Widget Study

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -15,59 +15,74 @@ class _MainPageState extends State<MainPage> {
     return Scaffold(
       appBar: AppBar(title: Text('카운터')),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              '숫자',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 40,
-              ),
-            ),
-            Text(
-              '$count',
-              style: TextStyle(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
                 color: Colors.red,
-                fontSize: 70,
+                width: 100,
+                height: 100,
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                print('ElevatedButton');
-              },
-              child: Text('ElevatedButton'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text('TextButton'),
-            ),
-            OutlinedButton(
-              onPressed: () {},
-              child: Text('OutlinedButton'),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: '글자', // 클릭전 힌트 형식에서 누르면 상단으로 이동
-                border: OutlineInputBorder(), // 외곽선
+              SizedBox(height: 30,),
+              Container(height: 30,),
+              Text(
+                '숫자',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 40,
+                ),
               ),
-              onChanged: (text) {
-                print(text);
-              },
-            ),
-            Image.network(
-              'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT15RrII6lkLU_aF06n_JjKUxYKIW_iPqwlj1cDw19tWV1asbxquziiHB6b34qqvDMKYLRD_g',
-              width: 100,
-              height: 100,
-              fit: BoxFit.fill,
-            ),
-            Image.asset(
-              'assets/image.png',
-              width: 100,
-              height: 100,
-              fit: BoxFit.fill,
-            ),
-          ],
+              Text(
+                '$count',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 70,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print('ElevatedButton');
+                },
+                child: Text('ElevatedButton'),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text('TextButton'),
+              ),
+              OutlinedButton(
+                onPressed: () {},
+                child: Text('OutlinedButton'),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: '글자', // 클릭전 힌트 형식에서 누르면 상단으로 이동
+                  border: OutlineInputBorder(), // 외곽선
+                ),
+                onChanged: (text) {
+                  print(text);
+                },
+              ),
+              Image.network(
+                'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT15RrII6lkLU_aF06n_JjKUxYKIW_iPqwlj1cDw19tWV1asbxquziiHB6b34qqvDMKYLRD_g',
+                width: 100,
+                height: 100,
+                fit: BoxFit.fill,
+              ),
+              Container(
+                color: Colors.red,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'assets/image.png',
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
